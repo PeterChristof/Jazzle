92a3aae9737e6ef86d2d12f2259cec1b81393a49
Peter Erian <peterchristof.erian@gmail.com>
2021-09-28 17:52:46 +0100

    first day back end

--- project-management-server/models/User.model.js
+++ project-management-server/models/User.model.js
@@ -1,13 +1,29 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const {Schema, model} = require("mongoose")
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const {ObjectId} = mongoose.Schema.Types
 const userSchema = new Schema({
-  username: {
-    type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
-  },
-  password: String,
-});
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    // resetToken:String,
+    // expireToken:Date,
+    pic:{
+     type:String,
+     default:"http://res.cloudinary.com/dedenco/image/upload/v1599511161/qxexqvsfekvmrupbfdqh.png"
+    },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+})
 
 const User = model("User", userSchema);
 
---
+++ project-management-server/routes/post.js
@@ -0,0 +1,82 @@
+const router = require("express").Router()
+const Post = require("../models/Post.model.js");
+const User = require("../models/User.model.js");
+const { post } = require("./index.js");
+// const fileUpload = require("../config/cloudinary");
+
+router.post("/post",async(req,res)=>{
+    const {title, description, songLink} = req.body;
+    if (!title || !description || !songLink) {
+        res.status(400).statusMessage({message: "missing fields - complete your post"});
+        return;
+    } try {
+        const response = await Post.create({title, description, songLink});
+        res.status(200).json(response);
+    } catch(e) {
+        res.status(500).json({message: e});
+    }
+});
+
+router.get("/post", async (req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(200).json(post);
+    } catch(e) {
+        res.status(500).json({message: e});
+    }
+});
+
+router.delete("/post/:id", async (req, res) => {
+    try {
+        await Post.findByIdAndRemove(req.params.id);
+        // if (post.userId == req.body.userId) {
+        //     await post.deleteOne()
+        // }
+        res.status(200).json({message: `Post with id ${req.params.id} was deleted`})
+    // } else { res.status(500).json("You can only delete your post")}
+    } catch(e) {
+        res.status(500).json({message: e});
+    }
+});
+
+router.put("/post/:id", async (req, res) => {
+    const {title, description, songLink} = req.body;
+try {
+    const response = await Post.findByIdAndUpdate(
+        req.params.id, {
+            title, description, songLink },
+            {new: true}
+    );
+    res.status(200).json(response);
+        } catch(e) {
+            console.log("error", e)
+            res.status(500).json({message:e.message});
+        }
+    });
+
+    // router.post("/upload", fileUpload.single("file"), (req, res) => {
+    //     try {
+    // res.status(200).json({ fileUrl: req.file.path });
+    //     } catch(e) {
+    // console.log("error", e);
+    // res.status(500).json({ message: e.message });
+    // }
+    // });
+
+//     router.put("/:id/like",async(req,res)=>{
+//         try{
+//             const post = await Post.findById(req.params.id)
+
+//             if(!post.likes.includes(req.body)){
+//                await post.updateOne({$push: {likes: req.body.userId}})
+//                res.status(200).json("post has been liked")
+//             }else{
+//                 await post.updateOne({$pull: {likes: req.body.userId}})
+//                 res.status(200).json("post has been disliked")
+//             }
+//         }catch(e){
+//             return res.status(500).json(e)
+//         }
+// })
+
+module.exports = router;
